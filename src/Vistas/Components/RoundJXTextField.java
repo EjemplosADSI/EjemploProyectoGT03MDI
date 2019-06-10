723cd8f6ff447b19fc0723d1a8df381b8971c4af
Diego Alonso Ojeda Medina <dojedam@sena.edu.co>
2019-06-10 14:38:38 -0500

    Login

---
+++ src/Vistas/Components/RoundJXTextField.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Vistas.Components;
+
+import java.awt.Graphics;
+import java.awt.Shape;
+import java.awt.geom.RoundRectangle2D;
+import org.jdesktop.swingx.JXTextField;
+
+/**
+ *
+ * @author Instructor
+ */
+public class RoundJXTextField extends JXTextField {
+    private Shape shape;
+    public RoundJXTextField(int size) {
+        //super(size);
+        setOpaque(false); // As suggested by @AVD in comment.
+    }
+
+    public RoundJXTextField() {
+        
+    }
+    protected void paintComponent(Graphics g) {
+         g.setColor(getBackground());
+         g.fillRoundRect(0, 0, getWidth()-1, getHeight()-1, 15, 15);
+         super.paintComponent(g);
+    }
+    protected void paintBorder(Graphics g) {
+         g.setColor(getBackground());
+         g.drawRoundRect(0, 0, getWidth()-1, getHeight()-1, 15, 15);
+    }
+    public boolean contains(int x, int y) {
+         if (shape == null || !shape.getBounds().equals(getBounds())) {
+             shape = new RoundRectangle2D.Float(0, 0, getWidth()-1, getHeight()-1, 15, 15);
+         }
+         return shape.contains(x, y);
+    }
+}
